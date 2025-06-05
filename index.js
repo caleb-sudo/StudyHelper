cab00fbb3d81835e18354ec21612143a2144e192
sucralfateoleophobicity <123672600+caleb-sudo@users.noreply.github.com>
2025-06-05 12:28:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -254,14 +254,7 @@ function buildQuestion() {
                 canvas.style.backgroundColor = "white";
                 canvas.style.width = "100%";
                 canvas.style.height = "100%";
-                /*let path1 = new Path2D();
-                path1.rect(10, 10, 100, 100);
-
-                let path2 = new Path2D(path1);
-                path2.moveTo(220, 60);
-                path2.arc(170, 60, 50, 0, 2 * Math.PI);
-
-                ctx.stroke(path2);*/
+                
                 let drawing = false;
                 function draw(event) {
                   if (!drawing) return;
